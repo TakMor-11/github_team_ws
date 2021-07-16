efe53eabba721c96805851caba8e0601d1de121d
TakMor-11 <takawall@icloud.com>
2021-07-16 20:19:16 +0900

    どやっ！笑

--- src/index.js
+++ src/index.js
@@ -132,6 +132,16 @@
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
+	function mul(){
+		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
+	}
+	function div(){
+		if (parseFloat(midNum[1].value) == 0){
+			eqlNum.value = "ERROR";
+		}else{
+			eqlNum.value = parseFloat(midNum[0].value) / parseFloat(midNum[1].value);
+		}
+	}
 
 	/* 演習の編集範囲 おわり */
 
